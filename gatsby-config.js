db9803bf004260b226f242a7b3d978d493fd6be5
Paul Balogh <paul.balogh@gmail.com>
2020-03-06 17:29:04 +0000

    Fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-google-analy`,
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
         head: false,
