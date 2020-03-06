72c02758dbbecc045150ade9888d1647923685ab
Cristian Dinu <cristian.dinu@learnfwd.com>
2020-03-06 17:26:24 +0000

    Typos and eslint

--- src/pages/submit.tsx
+++ src/pages/submit.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import ProductList from "../components/products/list";
-import SubscribeForm from "../components/subscribe";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -11,7 +9,7 @@ const IndexPage = ({ data }) => (
       <iframe
         className="airtable-embed airtable-dynamic-height"
         src="https://airtable.com/embed/shrvL17QCAuRqtYpA?backgroundColor=yellow"
-        frameborder="0" onmousewheel="" width="100%"
+        frameBorder="0" width="100%"
         height="1000"
         style={{
           border: "none",
