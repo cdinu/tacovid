47990aa9fda308ac624fa1f4e464952b40e715a4
Cristian Dinu <cristian.dinu@learnfwd.com>
2020-03-06 14:34:01 +0000

    Add `form-name` to please Gatsby and Netlify

--- src/components/subscribe.tsx
+++ src/components/subscribe.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default () => (
   <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
     <p>
       <label>Your Name: <input type="text" name="name" /></label>   
     </p>
