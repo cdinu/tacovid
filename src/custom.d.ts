4601cf838a58d7f3dc6085e0557ff4dfda6e969d
Paul Balogh <paul.balogh@gmail.com>
2020-03-07 00:17:36 +0000

    Sass lint

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,9 +1,2 @@
-declare module "*.svg" {
-  const content: string;
-  export default content;
-}
-
-declare module '*.scss' {
-  const content: {[className: string]: string};
-  export default content;
-}
+declare module "*.svg"
+declare module '*.scss'
