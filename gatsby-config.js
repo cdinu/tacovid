4756fd230bbaf8cce64fd819f8189abbb58aa5bd
Paul Balogh <paul.balogh@gmail.com>
2020-03-09 16:41:54 +0000

    Remove ref to twitter plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,6 @@ module.exports = {
         display: 'swap'
       }
     },
-    `gatsby-plugin-twitter`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
